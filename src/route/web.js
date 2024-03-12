027e7408fdec1d26c7823bc8cebd2a8a851b8932
Johny Ping <nqhitk20@gmail.com>
2024-03-12 21:51:13 +0700

    1

---
+++ src/route/web.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+let Router = express.Router();
+
+let initWebRoutes = (app) => {
+
+
+}
+
+module.exports = initWebRoutes;
---
+++ src/seeders/seeder-users.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'HoiDanit',
+      lastName: 'Eric',
+      email: 'example@example.com',
+      address: 'duoihamcau',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
